d827d4d7f615c98707737933448907cea23b0cef
JulianIrigoyen <julian@experoinc.com>
2022-01-12 17:07:59 -0300

    Control flow ok

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,81 @@
 use bracket_lib::prelude::*;
 
-
 /// BError allows to take advantag of "?" operator
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
 
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
 
 struct State {
+    mode: GameMode,
+}
 
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        //TODO: Fill in this stub
+        self.mode = GameMode::End;
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead...");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
 }
 
 impl GameState for State {
-    //! &mut self allows the tick gfunction to acces and change your State instance
+    //! &mut self allows the tick function to acces and change your State instance
     //! ctx provides a window into the currently running bracket-terminal,
     //!    and provides functions to interact with game display. fn cls clears the window
-    fn tick (&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.dead(ctx),
+            GameMode::End => self.play(ctx)
+        }
     }
+}
+
+/// Different Game Mode categories
+enum GameMode {
+    Menu,
+    Playing,
+    End,
 }
